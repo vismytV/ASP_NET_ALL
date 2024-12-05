a3cd240332bc9f1b1264f6aeb3e68dfc83779145
vismyt <vismyt2016@gmail.com>
2024-12-05 20:10:42 +0200

    DZ-1

---
+++ WebApplication1/WebApplication2/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WebApplication2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }//назва
+        public decimal Price { get; set; }//ціна
+        public string Description { get; set; }//опис
+        public DateTime ManufactureDate { get; set; }//дата виготовлення
+        public Category Category { get; set; } // Додаємо категорію
+    }
+}
