d9fa8a7107f9059337d86085766b1c3b227c0367
vismyt <vismyt2016@gmail.com>
2024-12-12 16:16:19 +0200

    Завдання 1

---
+++ zad1_v1/WebApplication2/WebApplication2/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductController: Controller
+    {
+        
+        public IActionResult ProductsList()
+        {
+
+            var category = new Category
+            {
+                Name = "Electronics",
+                Discount = 10,
+                Description = "Devices and gadgets"
+            };
+
+            var product = new Product
+            {
+                Name = "Smartphone",
+                Price = 699.99M,
+                Description = "High-end smartphone with AMOLED display.",
+                ManufactureDate = new DateTime(2023, 6, 15),
+                Category = category 
+            };
+
+            ViewBag.Description = product.Description;
+            ViewData["ManufactureDate"] = product.ManufactureDate.ToString("dd.MM.yyyy");
+
+            return View(product);
+         
+        }
+    }
+}
---
+++ zad1_v1/WebApplication2/WebApplication2/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }//Назва
+        public int Discount { get; set; } // Знижка у відсотках
+        public string Description { get; set; }//опис
+    }
+}
