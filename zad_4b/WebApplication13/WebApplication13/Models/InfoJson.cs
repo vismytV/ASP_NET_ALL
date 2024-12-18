de6b3e26139be42736cb66415e6dab9f77509fed
vismyt <vismyt2016@gmail.com>
2024-12-18 17:20:44 +0200

    Приклади API на власному проекті (проблема PUT вирішина)

---
+++ zad_4b/WebApplication13/WebApplication13/Models/InfoJson.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication13.Models
+{
+    public class InfoJson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ zad_4b/WebApplication13/WebApplication13/Views/CarServise/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication13.Views.CarServise
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
